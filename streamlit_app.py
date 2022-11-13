b453f3630799499fe1b71649d0479f4db7876064
AbhinavBM <115786840+AbhinavBM@users.noreply.github.com>
2022-11-13 12:22:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
-
+import streamlit as st
+st.set_page_config(page_title="My Webpage",page_icon=":tada:",layout="wide")
+st.subheader("HI,I am Abhinav :wave:")
+st.title("A Data Analyst From Germany")
+st.write("jsbbsxjsdhbncsdbjxk")
+st.write("jbshsjytrdvbjytdcvnjdsx jhf)
