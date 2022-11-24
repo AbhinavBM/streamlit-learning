8c5b6421831b35dd53cacf4fb4acfaeded406fc0
AbhinavBM <115786840+AbhinavBM@users.noreply.github.com>
2022-11-24 18:41:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,15 @@ import streamlit as st
 st.set_page_config(page_title="My Webpage",page_icon=":tada:",layout="wide")
 st.subheader("HI,I am Abhinav :wave:")
 st.title("A Data Analyst From Germany")
-st.write("jsbbsxjsdhbncsdbjxk")
-st.write("jbshsjytrdvbjytdcvnjdsx jhf")
+//st.write("jsbbsxjsdhbncsdbjxk")
+//st.write("jbshsjytrdvbjytdcvnjdsx jhf")
+data={
+  "Home":[x for x in range(1,11)],
+  "Proctorship":[x**2 for x in range(1,11)],
+  "Fee":[x for x in range(1,11)],
+  "Time table":[x for x in range(1,11)],
+  "logout":[x for x in range(1,11)]
+}
+
+df = pd.DataFrame(data =data)
+st.sidebar.seslctbox("Select  number ",[1,2,3,4,5])
